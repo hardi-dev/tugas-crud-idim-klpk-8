fdcae67a60e19ee6d30805bab3054f861a1b2936
hardi-dev <hardiansya94@gmail.com>
2023-02-06 00:08:16 +0700

    export db

---
+++ toko-kelpk-8.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 100421 (10.4.21-MariaDB)
+ Source Host           : localhost:3306
+ Source Schema         : toko
+
+ Target Server Type    : MySQL
+ Target Server Version : 100421 (10.4.21-MariaDB)
+ File Encoding         : 65001
+
+ Date: 06/02/2023 00:07:41
+*/
+
